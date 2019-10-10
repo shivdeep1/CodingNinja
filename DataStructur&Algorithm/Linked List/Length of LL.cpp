76c4aff8f94c7953f83f29f201a830e95539931b
shivdeep1 <shivdeepsachdeva@gmail.com>
2019-10-10 19:23:35 +0530

    commit

---
+++ DataStructur&Algorithm/Linked List/Length of LL.cpp
@@ -0,0 +1,47 @@
+QUES::
+
+Given a linked list, find and return the length of input LL. Do it iteratively.
+Input format :
+Linked list elements (separated by space and terminated by -1)
+Output format :
+Length of LL
+Sample Input :
+3 4 5 2 6 1 9 -1
+Sample Output :
+7
+
+
+SOL::
+
+/**********
+ * Following is the Node class that is already written.
+ 
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+ 
+*********/
+
+int length(Node *head) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input is handled automatically.
+     */
+    int count = 0;
+    
+    while( head != NULL){
+        count++;
+         head =  head -> next;
+    }
+    return count;
+    
+}
+
+
